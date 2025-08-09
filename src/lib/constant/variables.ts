284dd1e2aa0e0f0158ad7e5591730e503f01238b
Ebisa Galo <jimanuture@gmail.com>
2025-08-09 13:17:26 +0300

    update variables

--- src/lib/constant/variables.ts
+++ src/lib/constant/variables.ts
@@ -1,8 +1,9 @@
-export const BASE_URL = process.env.BASE_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 // Public images (served from /public/images)
 export const IMAGES = {
-  searchIcon: "/images/serachIcon.png", // filename as provided
+  searchIcon: "/images/serachIcon.png",
+  searchIconWhite: "/images/searchIconWhite.png",
   pickIcon: "/images/pickIcon.png",
   motorIcon: "/images/motorIcon.png",
   heroImage: "/images/heroImage.png",
